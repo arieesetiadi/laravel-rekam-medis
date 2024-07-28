abc97ea12790f84af53c0f217cfb9f1dce89307b
Arie Setiadi <arie@eannovate.com>
2024-07-28 10:54:48 +0800

    adjust dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,10 @@
 
 @section('content')
     <div class="d-flex justify-content-center align-items-center" style="height: 75vh">
-        <img src="{{ asset('assets/images/icons/neptune.png') }}" alt="Main">
+        <div>
+            <img width="150" height="150" src="{{ asset('assets/images/icons/admin.png') }}" alt="Admin Icon" class="mb-2">
+
+            <h1 class="text-uppercase text-center text-dark">Admin</h1>
+        </div>
     </div>
 @endsection
